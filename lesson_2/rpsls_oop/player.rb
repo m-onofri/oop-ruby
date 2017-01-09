a3e4262b17ac3a7ab7e148fcbcb594719c1649c2
Manuele Onofri <manuele.onofri@gmail.com>
2017-01-09 17:36:11 +0100

    Fix rubocop offensens for Player class

--- lesson_2/rpsls_oop/player.rb
+++ lesson_2/rpsls_oop/player.rb
@@ -12,7 +12,8 @@ class Human < Player
   def set_name
     user_name = nil
     loop do
-      prompt "Please enter your name (only alphanumerical characters are permitted):"
+      prompt "Please enter your name (only alphanumerical characters " \
+             "are allowed):"
       user_name = gets.chomp
       break if user_name =~ /^[A-Za-z0-9]+$/
     end
@@ -55,12 +56,13 @@ class Human < Player
 end
 
 class Computer < Player
-  attr_accessor :history, :freq
+  attr_accessor :history, :freq, :score
 
   def initialize(history)
+    @score = 0
     @history = history
     set_name
-    @freq = { "p"=>0.2, "s"=>0.2, "l"=>0.2, "k"=>0.2, "r"=>0.2 }
+    @freq = { "p" => 0.2, "s" => 0.2, "l" => 0.2, "k" => 0.2, "r" => 0.2 }
   end
 
   def choose
@@ -70,14 +72,14 @@ class Computer < Player
   private
 
   def weighted_sample
-    freq.max_by { |_, weight| rand ** (1.0 / weight) }.first
+    freq.max_by { |_, weight| rand**(1.0 / weight) }.first
   end
 
   def remove_loss_move(loss_move)
-      moves_array = Move::AVAILABLE_MOVES.keys.reject do |move| 
-        move == Move::AVAILABLE_MOVES.key(loss_move)
-      end
-      Move.new(moves_array.sample)
+    moves_array = Move::AVAILABLE_MOVES.keys.reject do |move|
+      move == Move::AVAILABLE_MOVES.key(loss_move)
+    end
+    Move.new(moves_array.sample)
   end
 end
 
@@ -100,11 +102,11 @@ class Hal < Computer
     comp_moves = history.player_moves_number
     loss_moves_freq = history.computer_moves_frequencies("loss")
     loss_move = loss_moves_freq.max_by { |_, freq| freq }
-    if comp_moves >= 5 && loss_move[1] > 0.2
-      self.move = remove_loss_move(loss_move[0])
-    else
-      self.move = Move.new(Move::AVAILABLE_MOVES.keys.sample)
-    end
+    self.move = if comp_moves >= 5 && loss_move[1] > 0.2
+                  remove_loss_move(loss_move[0])
+                else
+                  Move.new(Move::AVAILABLE_MOVES.keys.sample)
+                end
   end
 end
 
@@ -115,19 +117,18 @@ class Chappie < Computer
 
   def choose
     comp_moves = history.player_moves_number
-    if comp_moves % 5 == 0 && comp_moves % 2 == 1 && comp_moves > 1
+    if (comp_moves % 5).zero? && comp_moves.odd? && comp_moves > 1
       efficiency = history.moves_efficiency
       most_efficient_move = Move::AVAILABLE_MOVES.key(efficiency.first[0])
-      #less_efficient_move = Move::AVAILABLE_MOVES.key(efficiency.last[0])
       @freq.each do |move, _|
-        if move == most_efficient_move
-          @freq[move] = 0.60
-        else
-          @freq[move] = 0.10
-        end
+        @freq[move] = if move == most_efficient_move
+                        0.60
+                      else
+                        0.10
+                      end
       end
     end
-      self.move = Move.new(weighted_sample)
+    self.move = Move.new(weighted_sample)
   end
 end
 
@@ -137,10 +138,9 @@ class Sonnie < Computer
   end
 
   def choose
-    @freq = {"p"=>0.05, "s"=>0.05, "l"=>0.05, "k"=>0.05, "r"=>0.8}
+    @freq = { "p" => 0.05, "s" => 0.05, "l" => 0.05, "k" => 0.05, "r" => 0.8 }
     self.move = Move.new(weighted_sample)
   end
-
 end
 
 class Number5 < Computer
@@ -148,18 +148,3 @@ class Number5 < Computer
     @name = "Number 5"
   end
 end
-
-#freq = {"p"=>0.1, "s"=>0.1, "l"=>0.1, "k"=>0.2, "r"=>0.5}
-
-#def weighted_sample(freq)
-#  freq.max_by { |_, weight| rand ** (1.0 / weight) }.first
-#end
-
-#freqs = 100_000.times.each_with_object(Hash.new(0)) { |_, hash| hash[weighted_sample(freq)] += 1 }
-
-#p freqs.map { |move, count| [move, count / 100_000.0] }.to_h
-
-
-
-
-
