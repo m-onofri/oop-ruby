6d7471b53aceeb7feefb6460b2ff5f12d19f1765
Manuele Onofri <manuele.onofri@gmail.com>
2017-02-03 16:25:19 +0100

    New folder for TTT game

--- lesson_5/game.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'modules'
-require_relative 'board'
-require_relative 'player'
-require_relative 'score'
-require_relative 'setup_game'
-require_relative 'ttt_game'
-
-game = TTTGame.new
-game.play
